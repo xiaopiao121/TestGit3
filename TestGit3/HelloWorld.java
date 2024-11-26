154d251a391862160be8376661418fdb89ebd30d
liuchuang_loc <liuchuang_loc@163.com>
2024-11-26 14:11:15 +0800

    lc-学生提交了作业。

--- TestGit3/HelloWorld.java
+++ TestGit3/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
 		 System.out.println("hello world");
 		 System.out.println("我是老师，学生们在下面编写内容");
          System.out.println("学生修改了当前类");
+		 System.out.println("学生提交了作业");
 	 }
 }
